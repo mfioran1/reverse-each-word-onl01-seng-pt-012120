e71f2d94af4da9f249a31c12b44e64fa88ab92fd
Maurice Fiorani <mauricef97@gmail.com>
2020-01-31 06:30:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,21 @@
 def reverse_each_word(sentence1)
-#reverse_each_word = sentence1.split(" ")
+reverse_each_word = sentence1.split(" ")
 return_array = []
 reverse_each_word.each do |string|
 return_array << string.reverse
 end
-#return_array.join(" ")
+return_array.join(" ")
 end
 
 
 
 def reverse_each_word(sentence1)
-  #reverse_each_word = sentence1.split(" ") #turn string into an array
+  reverse_each_word = sentence1.split(" ") #turn string into an array
   test_array = []
   reverse_each_word.collect do|string| #iterate over the array
     test_array << string.reverse #reverse each word in the array
   end
-  #test_array.join(" ")
+  test_array.join(" ")
 end
 
 
