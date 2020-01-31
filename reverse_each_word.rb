ec3c08dda392023c8b1d549fe005b15e849b3907
Maurice Fiorani <mauricef97@gmail.com>
2020-01-31 05:50:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
 reverse_each_word = sentence1.split(" ")
 return_array = []
-reverse_each_word do |string|
+reverse_each_word.each do |string|
 return_array << string.reverse
 end
 return_array.join(" ")
